54949a925aa14543c7a5f62a407fc5408dd10a2d
jaehyuenjung <jaehyuen0613@gmail.com>
2022-01-31 14:08:44 +0900

    Counting Animation

--- src/components/Number.tsx
+++ src/components/Number.tsx
@@ -1,4 +1,19 @@
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
 import styled from "styled-components";
+
+const COLOR_LIST = [
+    "#d63031",
+    "#e17055",
+    "#fdcb6e",
+    "#00b894",
+    "#78e08f",
+    "#00cec9",
+    "#0984e3",
+    "#6c5ce7",
+    "#8e44ad",
+    "#1e3799",
+];
 
 const Wrapper = styled.div`
     width: 160px;
@@ -9,16 +24,23 @@ const Wrapper = styled.div`
     align-items: center;
 `;
 
-const Card = styled.div`
+const Card = styled(motion.div)<{ index: number }>`
     width: 100%;
     height: 100%;
     display: grid;
     grid-template-rows: repeat(2, 1fr);
     align-items: center;
     position: absolute;
+    transform: perspective(500px);
+    transform-style: preserve-3d;
+    z-index: ${(props) => props.index};
+
+    * {
+        backface-visibility: hidden;
+    }
 `;
 
-const Row = styled.div`
+const Row = styled(motion.div)`
     width: 100%;
     height: 100%;
     position: relative;
@@ -41,7 +63,7 @@ const Row = styled.div`
     }
 `;
 
-const ValueWrap = styled.div`
+const ValueWrap = styled(motion.div)`
     display: flex;
     position: absolute;
     align-items: center;
@@ -63,27 +85,88 @@ const Value = styled.div`
     color: white;
 `;
 
+const cardVariants = {
+    entry: {},
+    visible: {},
+    exit: {},
+};
+
 interface IParams {
     value: number;
 }
 
 function Number({ value }: IParams) {
+    const pre = (10 - value + 1) % 10;
+    const next = (value + 1) % 10;
+    const [back] = useState(false);
+
+    const topVariants = {
+        entry: (back: boolean) => ({
+            rotateX: back ? -180 : 0,
+            opacity: 1,
+            backgroundColor: back ? COLOR_LIST[pre] : COLOR_LIST[next],
+        }),
+        visible: { rotateX: 0, backgroundColor: COLOR_LIST[value] },
+        exit: (back: boolean) => ({
+            rotateX: back ? 0 : -180,
+            opacity: 0,
+            backgroundColor: back ? COLOR_LIST[next] : COLOR_LIST[pre],
+        }),
+    };
+
+    const bottomVariants = {
+        entry: (back: boolean) => ({
+            rotateX: back ? 0 : 180,
+            opacity: 1,
+            backgroundColor: back ? COLOR_LIST[next] : COLOR_LIST[pre],
+        }),
+        visible: { rotateX: 0, backgroundColor: COLOR_LIST[value] },
+        exit: (back: boolean) => ({
+            rotateX: back ? 180 : 0,
+            opacity: 0,
+            backgroundColor: back ? COLOR_LIST[pre] : COLOR_LIST[next],
+        }),
+    };
+
     return (
         <Wrapper>
-            <Card>
-                <Row>
-                    <ValueWrap>
-                        <Value>{value}</Value>
-                    </ValueWrap>
-                </Row>
-                <Row>
-                    <ValueWrap>
-                        <Value>{value}</Value>
-                    </ValueWrap>
-                </Row>
-            </Card>
+            <AnimatePresence initial={false}>
+                <Card
+                    key={value}
+                    variants={cardVariants}
+                    initial="entry"
+                    animate="visible"
+                    exit="exit"
+                    index={10 - value}
+                >
+                    <Row
+                        custom={back}
+                        variants={topVariants}
+                        transition={{
+                            type: "tween",
+                            duration: 0.5,
+                        }}
+                    >
+                        <ValueWrap>
+                            <Value>{value}</Value>
+                        </ValueWrap>
+                    </Row>
+                    <Row
+                        custom={back}
+                        variants={bottomVariants}
+                        transition={{
+                            type: "tween",
+                            duration: 0.5,
+                        }}
+                    >
+                        <ValueWrap>
+                            <Value>{value}</Value>
+                        </ValueWrap>
+                    </Row>
+                </Card>
+            </AnimatePresence>
         </Wrapper>
     );
 }
 
-export default Number;
+export default React.memo(Number);
