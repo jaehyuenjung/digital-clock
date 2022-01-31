95d50c8018b1864860c15f356455146db798cede
jaehyuenjung <jaehyuen0613@gmail.com>
2022-01-31 14:11:54 +0900

    Convert to clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Number from "./components/Number";
@@ -11,12 +12,48 @@ const Wrapper = styled.div`
     flex-direction: column;
 `;
 
+const NumberWrap = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Column = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+`;
+
+const DobuleDot = styled(motion.div)`
+    width: 80px;
+    height: 120px;
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+    justify-items: center;
+    align-items: center;
+    border-radius: 50%;
+
+    &::before,
+    &::after {
+        content: "";
+        width: 35px;
+        height: 35px;
+        border-radius: inherit;
+        background-color: black;
+    }
+`;
+
 function App() {
     const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
     useEffect(() => {
         const getCurTime = () => {
             const date = new Date();
             setSeconds(date.getSeconds());
+            setMinutes(date.getMinutes());
+            setHours(date.getHours());
         };
 
         getCurTime();
@@ -24,7 +61,28 @@ function App() {
     }, []);
     return (
         <Wrapper>
-            <Number value={seconds % 10} />
+            <NumberWrap>
+                <Column>
+                    <Number value={~~(hours / 10)} />
+                    <Number value={hours % 10} />
+                </Column>
+                <DobuleDot
+                    whileHover={{ borderRadius: "0%" }}
+                    transition={{ duration: 0.5 }}
+                />
+                <Column>
+                    <Number value={~~(minutes / 10)} />
+                    <Number value={minutes % 10} />
+                </Column>
+                <DobuleDot
+                    whileHover={{ borderRadius: "0%" }}
+                    transition={{ duration: 0.5 }}
+                />
+                <Column>
+                    <Number value={~~(seconds / 10)} />
+                    <Number value={seconds % 10} />
+                </Column>
+            </NumberWrap>
         </Wrapper>
     );
 }
